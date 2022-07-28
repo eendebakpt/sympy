e3b64b4dfd59df356aea30150463693c7f3fd63d
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-07-28 21:54:17 +0200

    lazy import of printing functions

--- sympy/printing/__init__.py
+++ sympy/printing/__init__.py
@@ -1,48 +1,49 @@
 """Printing subsystem"""
 
+from sympy.core.cache import lazy_function
+
 from .pretty import pager_print, pretty, pretty_print, pprint, pprint_use_unicode, pprint_try_use_unicode
 
 from .latex import latex, print_latex, multiline_latex
 
-from .mathml import mathml, print_mathml
+mathml = lazy_function('sympy.printing.mathml', 'mathml')
+print_mathml = lazy_function('sympy.printing.mathml', 'print_mathml')
+print_python = lazy_function('sympy.printing.python', 'print_python')
+python = lazy_function('sympy.printing.python', 'python')
+pycode = lazy_function('sympy.printing.pycode', 'pycode')
 
-from .python import python, print_python
-
-from .pycode import pycode
 
 from .codeprinter import print_ccode, print_fcode
 
 from .codeprinter import ccode, fcode, cxxcode # noqa:F811
 
-from .glsl import glsl_code, print_glsl
 
-from .rcode import rcode, print_rcode
+glsl_code = lazy_function('sympy.printing.glsl', 'glsl_code')
+print_glsl = lazy_function('sympy.printing.glsl', 'print_glsl')
 
-from .jscode import jscode, print_jscode
+rcode = lazy_function('sympy.printing.rcode', 'rcode')
+print_rcode = lazy_function('sympy.printing.rcode', 'print_rcode')
+jscode = lazy_function('sympy.printing.jscode', 'jscode')
+print_jscode = lazy_function('sympy.printing.jscode', 'print_jscode')
 
-from .julia import julia_code
 
-from .mathematica import mathematica_code
+julia_code = lazy_function('sympy.printing.julia', 'julia_code')
+mathematica_code = lazy_function('sympy.printing.mathematica', 'mathematica_code')
+octave_code = lazy_function('sympy.printing.octave', 'octave_code')
+rust_code = lazy_function('sympy.printing.rust', 'rust_code')
+print_gtk = lazy_function('sympy.printing.gtk', 'print_gtk')
+preview = lazy_function('sympy.printing.preview', 'preview')
+srepr = lazy_function('sympy.printing.repr', 'srepr')
+print_tree = lazy_function('sympy.printing.tree', 'print_tree')
 
-from .octave import octave_code
-
-from .rust import rust_code
-
-from .gtk import print_gtk
-
-from .preview import preview
-
-from .repr import srepr
-
-from .tree import print_tree
+dotprint = lazy_function('sympy.printing.dot', 'dotprint')
+maple_code = lazy_function('sympy.printing.maple', 'maple_code')
+print_maple_code = lazy_function('sympy.printing.maple', 'print_maple_code')
 
 from .str import StrPrinter, sstr, sstrrepr
 
 from .tableform import TableForm
 
-from .dot import dotprint
-
-from .maple import maple_code, print_maple_code
 
 __all__ = [
     # sympy.printing.pretty
